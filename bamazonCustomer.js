14b9cf4a9504ade324f321599871b3108001d26e
Mike Kravtsov <mike4040@me.com>
2019-10-01 09:59:15 -0700

    Basic validation

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const inquirer = require('inquirer');
 
 let item_id;
-let inventory = {};
+let inv = {};
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -14,7 +14,7 @@ const connection = mysql.createConnection({
 
 connection.connect(err => {
   if (err) throw err;
-  getItems();
+  else getItems();
 });
 
 function getItems() {
@@ -28,23 +28,23 @@ function renderItems(err, res) {
   if (err) throw err;
   // console.table(res);
   console.log('ID  Name           Price');
-  res.forEach(e => {
-    if (e.stock_quantity > 0) {
-      inventory[e.item_id] = e.stock_quantity;
-      console.log(`${e.item_id}  | ${e.product_name} | ${e.price}`);
+  res.forEach(item => {
+    if (item.stock_quantity > 0) {
+      inv[item.item_id] = item.stock_quantity;
+      console.log(`${item.item_id}  | ${item.product_name} | ${item.price}`);
     }
   });
+  console.log(inv);
   askItem();
 }
 function askItem() {
   inquirer
     .prompt({
       name: 'item_id',
-      type: 'number',
+      type: 'input',
       message: 'Please enter ID of item you want to buy OR 0 for EXIT',
       validate: function(input) {
-        console.log('Validating ID input', input, console.log(inventory));
-        if (input === 0 || inventory.hasOwnProperty([input])) return true;
+        if (input === 0 || inv.hasOwnProperty(input)) return true;
         else return 'Please select item from the list';
       }
     })
@@ -65,7 +65,7 @@ function askQuantity(answer) {
 }
 
 function sellItem(answer) {
-  let remaining = inventory[item_id] - answer.quantity;
+  let remaining = inv[item_id] - answer.quantity;
   if (remaining < 0) {
     console.log('Insufficient quantity!');
     askItem();
